2379474effd5aef092f9923ac35a3d3286bb78d5
linus5178 <109713775+linus5178@users.noreply.github.com>
2025-04-03 02:36:04 +0530

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,6 @@
+num1=int(input("enter the first number: "))
+num2=int(input("enter the second number: "))
+print('Addition: ', num1+num2)
+print('Subtraction: ', num1-num2)
+print('Multiplication: ', num1*num2)
+print('Division: ', num1/num2)
